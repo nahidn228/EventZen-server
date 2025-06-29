4d239bbd3c7595da82852a1cfacc5161b35b1154
nahidn228 <nahidn228@gmail.com>
2025-06-30 01:13:30 +0600

    fix some bug and deploy

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const user_controller_1 = require("./app/controllers/user.controller");
+const events_controller_1 = require("./app/controllers/events.controller");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use("/events", events_controller_1.eventsRoutes);
+app.use("/users", user_controller_1.usersRoutes);
+app.get("/", (req, res) => {
+    res.send("Welcome to EventZen Server");
+});
+exports.default = app;
---
+++ src/app/models/events.models.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Event = void 0;
+const mongoose_1 = require("mongoose");
+const eventSchema = new mongoose_1.Schema({
+    eventTitle: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true },
+    location: { type: String, required: true },
+    description: { type: String },
+    attendeeCount: { type: Number },
+}, {
+    versionKey: false,
+    timestamps: true,
+});
+exports.Event = (0, mongoose_1.model)("Event", eventSchema);
